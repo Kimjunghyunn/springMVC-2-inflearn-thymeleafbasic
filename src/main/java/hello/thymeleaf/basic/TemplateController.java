ab77416c95d7206fc75542283716e0936cb0ce5c
Kimjunghyunn <93319948+Kimjunghyunn@users.noreply.github.com>
2022-05-27 16:58:23 +0900

    템플릿 조각-오타수정

--- src/main/java/hello/thymeleaf/basic/TemplateController.java
+++ src/main/java/hello/thymeleaf/basic/TemplateController.java
@@ -1,5 +1,4 @@
 package hello.thymeleaf.basic;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/template")
 public class TemplateController {
-    @GetMapping("/template")
+    @GetMapping("/fragment")
     public String template() {
         return "template/fragment/fragmentMain";
     }
